e954d551bea9e953fea5fa1a71003e35f511c5d8
54hanyi <tp6c04u4456@yahoo.com.tw>
2023-11-09 13:26:35 +0800

    Fix Show to show

--- src/App.js
+++ src/App.js
@@ -1,53 +1,51 @@
-import { useState, useEffect, useRef } from 'react';
-import { HashRouter, Route, Routes, Link } from 'react-router-dom';
-import Navbar from './views/Navbar';
-import Home from './views/Home';
-import About from './views/About';
-import Portfolio from './views/Portfolio';
-import Contact from './views/Contact';
-import Footer from './views/Footer';
+import { useState, useEffect, useRef } from "react";
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
+import Navbar from "./views/Navbar";
+import Home from "./views/Home";
+import About from "./views/About";
+import Portfolio from "./views/Portfolio";
+import Contact from "./views/Contact";
+import Footer from "./views/Footer";
 
-import ScrollToTopButton from './components/ScrollToTopButton';
-import FullscreenAnimation from './components/FullscreenAnimation';
-
+import ScrollToTopButton from "./components/ScrollToTopButton";
+import FullscreenAnimation from "./components/FullscreenAnimation";
 
 function App() {
   const [animationVisible, setAnimationVisible] = useState(true);
-  const [Show, setShow] = useState(false);
-  const [theme, setTheme] = useState('light'); 
+  const [show, setShow] = useState(false);
+  const [theme, setTheme] = useState("light");
 
   const navRef = useRef(null);
 
-
   const setDarkTheme = () => {
-    document.querySelector('body').setAttribute('data-theme', 'dark');
+    document.querySelector("body").setAttribute("data-theme", "dark");
     if (navRef.current) {
-      navRef.current.setAttribute('data-theme', 'dark');
+      navRef.current.setAttribute("data-theme", "dark");
     }
-    localStorage.setItem('selectedTheme', 'dark');
+    localStorage.setItem("selectedTheme", "dark");
   };
-    
+
   const setLightTheme = () => {
-      document.querySelector('body').setAttribute('data-theme', 'light');
-      if (navRef.current) {
-        navRef.current.setAttribute('data-theme', 'light');
-      }
-      localStorage.setItem('selectedTheme', 'light');
+    document.querySelector("body").setAttribute("data-theme", "light");
+    if (navRef.current) {
+      navRef.current.setAttribute("data-theme", "light");
+    }
+    localStorage.setItem("selectedTheme", "light");
   };
 
   const toggleTheme = () => {
-      if (theme === 'light') {
-          setTheme('dark');
-          setDarkTheme();
-          localStorage.setItem('selectedTheme', 'dark');
-      } else {
-          setTheme('light');
-          setLightTheme();
-          localStorage.setItem('selectedTheme', 'light');
-      }
+    if (theme === "light") {
+      setTheme("dark");
+      setDarkTheme();
+      localStorage.setItem("selectedTheme", "dark");
+    } else {
+      setTheme("light");
+      setLightTheme();
+      localStorage.setItem("selectedTheme", "light");
+    }
   };
 
-  const selectedTheme = localStorage.getItem('selectedTheme');
+  const selectedTheme = localStorage.getItem("selectedTheme");
   useEffect(() => {
     // 在元件掛載時，檢查 localStorage 中是否儲存了使用者選擇的主題，讓網頁重新整理後，圖片也能跟著主題改變
     if (selectedTheme) {
@@ -56,8 +54,8 @@ function App() {
   }, []);
 
   // 用來改變主題
-  if(selectedTheme === 'dark') {
-      setDarkTheme();
+  if (selectedTheme === "dark") {
+    setDarkTheme();
   }
 
   // 這是用於在動畫完成後執行的函數
@@ -73,29 +71,38 @@ function App() {
     }
   }, [animationVisible]);
 
-
   return (
     <HashRouter>
-      
-      <div className='page-container'>
-      <div className='content-wrap'>
-        {animationVisible ? (
-          <FullscreenAnimation onHide={handleHideAnimation} theme={theme} />
-        ) : (
-          <Link to="/" />
+      <div className="page-container">
+        <div className="content-wrap">
+          {animationVisible ? (
+            <FullscreenAnimation onHide={handleHideAnimation} theme={theme} />
+          ) : (
+            <Link to="/" />
+          )}
+          {show && (
+            <Navbar
+              className="Navbar"
+              theme={theme}
+              toggleTheme={toggleTheme}
+            />
+          )}{" "}
+          {/* 檢查 Show 的值，如果為 true，則顯示 Navbar */}
+          {show && (
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/portfolio" element={<Portfolio />} theme={theme} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          )}
+        </div>
+        {show && (
+          <div>
+            <ScrollToTopButton />
+            <Footer />
+          </div>
         )}
-        {Show && <Navbar className='Navbar' theme={theme} toggleTheme={toggleTheme} />}  {/* 檢查 Show 的值，如果為 true，則顯示 Navbar */}
-        {Show && <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} theme={theme} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>}
-      </div>
-      {Show && <div>
-        <ScrollToTopButton />
-        <Footer /> 
-      </div>}
       </div>
     </HashRouter>
   );
